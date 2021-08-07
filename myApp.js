ade3096ae9cc9edae8bf51707b4a69078c647793
merlinjones10 <merlinjones10@gmail.com>
2021-08-07 15:30:21 +0100

    added env

--- myApp.js
+++ myApp.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
+require("dotenv").config();
 
-// console.log("Hello Express");
-
+console.log(process.env.MESSAGE_STYLE);
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({
+      message: "Hello Json",
+    });
+  }
 });
+
 app.get("/", (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
